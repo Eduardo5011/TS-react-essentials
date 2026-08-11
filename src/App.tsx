bb105bba12f1840152f44969339ccca2f4a366de
Eduardo5011 <eduardo.rosado5011@gmail.com>
2026-08-11 17:55:33 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Component from './starter/02-props'
+
+function App() {
+  return (
+    <main>
+      <Component name='peter' id={123}>
+        <h2>Hello world</h2>
+        </Component>
+        <Component name='peter' id={123} />
+    </main>
+  );
+}
+
+export default App;
