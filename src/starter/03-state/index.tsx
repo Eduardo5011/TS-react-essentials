30cc734bd5de0965b53933b5d743d762e9b4bd13
Eduardo5011 <eduardo.rosado5011@gmail.com>
2026-08-12 18:13:56 -0400

    updated index.tsx

--- src/starter/03-state/index.tsx
+++ src/starter/03-state/index.tsx
@@ -1,8 +1,48 @@
+import { useState } from "react";
+
+type Link = {
+  id: string;
+  url: string;
+  text: string;
+};
+
+const navLinks: Link[] = [
+  {
+    id: 1,
+    url: "some url",
+    text: "some text",
+  },
+  {
+    id: 2,
+    url: "some url",
+    text: "some text",
+  },
+  {
+    id: 3,
+    url: "some url",
+    text: "some text",
+  },
+];
+
 function Component() {
+  const [text, setText] = useState("ShakeAndbake");
+  const [number, setNumber] = useState(1);
+  const [list, setList] = useState<string[]>([]);
+  const [links, setLinks] = useState<Link[]>(navLinks);
   return (
     <div>
-      <h2>React & Typescript</h2>
-      <h2>State</h2>
+      <h2 className="mb-1">React & Typescript</h2>
+      <button
+        className="btn btn-center"
+        onClick={() => {
+          // setText(1);
+          setNumber(23);
+          // setList([1,2])
+          setLinks([...links,{id:4, url:'hello',text:'hello'}])
+        }}
+      >
+        click me
+      </button>
     </div>
   );
 }
