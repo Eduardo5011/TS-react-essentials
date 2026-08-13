692b823e7afb21867bdb2b73c421d26cac683b5f
Eduardo5011 <eduardo.rosado5011@gmail.com>
2026-08-13 19:42:56 -0400

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import Component from './starter/04-events'
+// import Component from './final/05-challenge'
+import Component from './starter/05-challenge'
 
 function App() {
   return (
     <main>
-      <Component/>
+      {/* <Component type="basic" name="susan"/>
+      <Component type="advanced" name="anna" email="anan@gmail.com"/> */}
+      <Component type="basic" name="susan"/>
+      <Component type="advanced" name="susan" email="anna@anna.com"/>
     </main>
   );
 }
