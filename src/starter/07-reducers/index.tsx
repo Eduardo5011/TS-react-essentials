17281cc4cc83dbe616fd9bd364a85fffe059ad63
Eduardo5011 <eduardo.rosado5011@gmail.com>
2026-08-19 23:20:02 -0400

    updated index reducers

--- src/starter/07-reducers/index.tsx
+++ src/starter/07-reducers/index.tsx
@@ -1,33 +1,33 @@
 import { useReducer } from "react";
-import {initialState} from './reducer'
+import {initialState, counterReducer} from './reducer'
 function Component() {
   const [state, dispatch] = useReducer(counterReducer, initialState)
   return (
     <div>
-      <h2>Count: 0</h2>
-      <h2>Status: Active</h2>
+      <h2>Count: {state.count}</h2>
+      <h2>Status: {state.status}</h2>
 
       <div className='btn-container'>
-        <button onClick={() => console.log('increment')} className='btn'>
+        <button onClick={() => dispatch({type: 'increment'})} className='btn'>
           Increment
         </button>
-        <button onClick={() => console.log('decrement')} className='btn'>
+        <button onClick={() => dispatch({type: 'decrement'})} className='btn'>
           Decrement
         </button>
-        <button onClick={() => console.log('reset')} className='btn'>
+        <button onClick={() => dispatch({type: 'reset'})} className='btn'>
           Reset
         </button>
       </div>
       <div className='btn-container'>
         <button
-          onClick={() => console.log('set status to active')}
+          onClick={() => dispatch({type: 'setStatus', payload: 'active'})}
           className='btn'
         >
           Set Status to Active
         </button>
         <button
           className='btn'
-          onClick={() => console.log('set status to inactive')}
+          onClick={() => dispatch({type: 'setStatus', payload: 'inactive'})}
         >
           Set Status to Inactive
         </button>
--- src/starter/07-reducers/reducer.tsx
+++ src/starter/07-reducers/reducer.tsx
@@ -8,6 +8,32 @@ export const initialState: CounterState = {
   status: "Pending",
 };
 
-export const counterReducer = (state:CounterState, action:any):CounterState => {
-return state;
-}
+type UpdateCountAction = {
+  type: "increment" | "decrement" | "reset";
+};
+
+type SetStatusAction = {
+  type: "setStatus";
+  payload: "active" | "inactive";
+};
+
+type CounterAction = UpdateCountAction | SetStatusAction;
+
+export const counterReducer = (
+  state: CounterState,
+  action: CounterAction,
+): CounterState => {
+  switch (action.type) {
+    case "increment":
+      return { ...state, count: state.count + 1 };
+    case "decrement":
+      return { ...state, count: state.count - 1 };
+    case "reset":
+      return { ...state, count: 0 };
+    case "setStatus":
+      return { ...state, status: action.payload };
+
+    default:
+      return state;
+  }
+};
